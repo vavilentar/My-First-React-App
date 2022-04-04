bfcb56417d22860d0ffaf4eaecf0f171fe35958c
Roman Pchelkin <epos.91@mail.ru>
2022-04-04 21:08:48 +0300

    Update employees-add-form.js

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -41,13 +41,15 @@ class EmployeesAddFrom extends Component{
 						placeholder="Как его зовут?"
 						name="name" 
 						value={name}
-						onChange={this.onValueChange}/>
+						onChange={this.onValueChange}
+						required/>
 					<input type="number" 
 						className="form-control new-post-label"
 						placeholder="З/П в $?"
 						name="salary" 
 						value={salary}
-						onChange={this.onValueChange}/>
+						onChange={this.onValueChange}
+						required/>
 					<button type="submit"
 						className="btn btn-outline-light">Добавить</button>
 				</form>
