6f470c38e6712d8ca5be23b047f35ee1df009af7
Roman Pchelkin <epos.91@mail.ru>
2022-04-04 10:29:12 +0300

    My-First-React-App

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+	return (
+		<input type="text" 
+		className="form-control search-input"
+		placeholder="Найти сотрудника"/> //Готовые классы из библиотеки Bootstrap
+	);
+};
+
+export default SearchPanel;
